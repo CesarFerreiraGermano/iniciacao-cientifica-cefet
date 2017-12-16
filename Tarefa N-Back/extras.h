e0063e56246a3a32d00b81ce69943e121c6da737
César Ferreira Germano <cesar.ferreira.germano@gmail.com>
2017-12-16 00:26:35 -0200

    Identação e padronização de 'extras.h'

--- Tarefa N-Back/extras.h
+++ Tarefa N-Back/extras.h
@@ -1,28 +1,25 @@
+void read_conf_file(){ // Leitura de parâmetros no arquivo "config.txt"
+    char *cmd;
+    char buf[200];
+    FILE *arq;
 
-void read_conf_file(){
-  char *cmd;
-  char buf[200];
-  FILE *arq;
-
-  arq = fopen("config.txt","r");
- if (arq == NULL){
-    printf("\nErro ao abrir arquivo de configuracao\n");
-    return;
-  }
-  if (fgets(buf,200,arq))
-    ;
-  while (!feof(arq))
-  {
-    cmd = strtok(buf,"=");
-    int val = atoi(strtok(NULL,";"));
+    arq = fopen("config.txt","r");
+    if (arq == NULL){
+        printf("\nErro ao abrir arquivo de configuracao\n");
+        return;
+    }
+    if (fgets(buf,200,arq))
+        ;
+    while (!feof(arq)){
+        cmd = strtok(buf,"=");
+        int val = atoi(strtok(NULL,";"));
 		if (strcmp(cmd,"evolution") == 0)
-			if (val==0)
-			{
-				loadPopulationFromFile=false;
-				 trials=1;
-				 loadBestGeneFromFile=true;
-				 logDynamic=true;
-				 break;
+		    if (val==0){
+                loadPopulationFromFile=false;
+				trials=1;
+				loadBestGeneFromFile=true;
+				logDynamic=true;
+			    break;
 			}
 		if (strcmp(cmd,"loadPopulationFromFile") == 0)
 			loadPopulationFromFile=val;
@@ -36,9 +33,8 @@ void read_conf_file(){
 			end=val;
 		if (strcmp(cmd,"start")==0)
 			start=val;
-    if(fgets(buf,200,arq));//Uso de if para evitar warning com compilacao -Ofast
-
-  }
-  //printf("%d %d %d %d\n",loadPopulationFromFile,trials,logDynamic,loadBestGeneFromFile);
-  fclose(arq);
+        if(fgets(buf,200,arq))
+            ; //Uso de if para evitar warning com compilação -Ofast
+    }
+    fclose(arq);
 }
